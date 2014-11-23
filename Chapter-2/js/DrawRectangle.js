2e48404712adfd0f2ad20263395ca11807da1490
zhangsq <zhangsq@outlook.com>
2014-11-23 23:16:25 +0800

    1、增加忽略文件列表 2、使用Canvas绘制矩形示例 2、WebGL第一个示例

---
+++ Chapter-2/js/DrawRectangle.js
@@ -0,0 +1,6 @@
+function main() {
+    var ctx = document.querySelector('#example').getContext('2d');
+
+    ctx.fillStyle = 'rgba(0, 0, 255, 255)';
+    ctx.fillRect(120, 10, 150, 150);
+}
---
+++ Chapter-2/js/HelloCanvas.js
@@ -0,0 +1,9 @@
+function main() {
+    var gl = getWebGLContext(document.querySelector('#webgl'));
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+    gl.clearColor(0, 0, 0, 255);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
