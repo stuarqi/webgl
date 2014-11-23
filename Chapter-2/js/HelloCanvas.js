741fc859a96f315228ae66cbe7499e8732b9de92
zhangsq <zhangsq@outlook.com>
2014-11-23 23:22:02 +0800

    测试透明度

--- Chapter-2/js/HelloCanvas.js
+++ Chapter-2/js/HelloCanvas.js
@@ -4,6 +4,6 @@ function main() {
         console.log('Failed to get the rendering context for WebGL');
         return;
     }
-    gl.clearColor(0, 0, 0, 255);
+    gl.clearColor(0, 0, 0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
