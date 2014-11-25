9c80f4ff438ea79a0b3aa727e0d528d8b3b0b597
zhangsq <zhangsq@mapbar.com>
2014-11-25 14:36:10 +0800

    '平移'

---
+++ Chapter-3/js/TranslateTriangle.js
@@ -0,0 +1,61 @@
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'uniform vec4 u_Translation;\n' +
+    'void main() {\n' +
+    '   gl_Position = a_Position + u_Translation;\n' +
+    '}\n';
+
+var FSHADER_SOURCE =
+    'precision mediump float;\n' +
+    'void main() {\n' +
+    '   gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n' +
+    '}\n';
+
+function main() {
+    var canvas = document.querySelector('#webgl'),
+        gl = getWebGLContext(canvas);
+
+    if (!gl) {
+        console.log('Failed to get rendering context for WebGL');
+        return;
+    }
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders');
+        return;
+    }
+    var n = initVertexBuffer(gl);
+    if (n < 0) {
+        console.log('Failed to set positions of the vertex');
+        return;
+    }
+
+    var u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
+    gl.uniform4f(u_Translation, 0.5, 0.5, 0, 0.0);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+function initVertexBuffer(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, 0.5, -0.5
+    ]);
+    var n = 3;
+
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create buffer object');
+        return -1;
+    }
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
